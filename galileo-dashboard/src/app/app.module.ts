f87777954e0ea36b6207eba198430a7f6ad560d1
praith <e1425076@student.tuwien.ac.at>
2019-07-03 21:52:46 +0200

    Import bootstrap styles and ng-bootstrap module

--- galileo-dashboard/src/app/app.module.ts
+++ galileo-dashboard/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
