6bda3b99129a44f09a40cdc2a0b3cc2cd6e7b9c8
Thomas Rausch <thomas@rauschig.org>
2019-07-24 17:05:11 -0400

    Replace falcon_cors with simpler custom component

--- galileo/webapp/app.py
+++ galileo/webapp/app.py
@@ -4,7 +4,6 @@ import os
 
 import falcon
 import redis
-from falcon_cors import CORS
 from symmetry import eventbus
 from symmetry.eventbus.redis import RedisConfig
 
@@ -117,6 +116,34 @@ def init_context():
     return context
 
 
-cors = CORS(allow_origins_list=['http://localhost:63342'])
-api = falcon.API(middleware=[cors.middleware])
+class CORSComponent(object):
+    """
+    CORS preprocessor from the Falcon documentation.
+    """
+    def process_response(self, req, resp, resource, req_succeeded):
+        resp.set_header('Access-Control-Allow-Origin', '*')
+
+        if (req_succeeded
+                and req.method == 'OPTIONS'
+                and req.get_header('Access-Control-Request-Method')
+        ):
+            # NOTE(kgriffs): This is a CORS preflight request. Patch the
+            #   response accordingly.
+
+            allow = resp.get_header('Allow')
+            resp.delete_header('Allow')
+
+            allow_headers = req.get_header(
+                'Access-Control-Request-Headers',
+                default='*'
+            )
+
+            resp.set_headers((
+                ('Access-Control-Allow-Methods', allow),
+                ('Access-Control-Allow-Headers', allow_headers),
+                ('Access-Control-Max-Age', '86400'),  # 24 hours
+            ))
+
+
+api = falcon.API(middleware=[CORSComponent()])
 setup(api, init_context())
