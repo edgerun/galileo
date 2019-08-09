2a5d64a568ac49c213ca57b990bae2c1a5a76262
Thomas Rausch <thomas@rauschig.org>
2019-08-09 17:28:41 -0400

    Enable logging when starting with gunicorn

--- galileo/webapp/gunicorn.conf.py
+++ galileo/webapp/gunicorn.conf.py
@@ -1,5 +1,9 @@
+import logging
+
+
 def on_starting(server):
-    pass
+    # simple way of getting the logs to output by reusing gunicorn's --log-level config
+    logging.basicConfig(level=logging.getLogger('gunicorn.error').level)
 
 
 def on_exit(server):
