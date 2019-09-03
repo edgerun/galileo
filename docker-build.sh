9e3721fa535bf579726d43a49370bd57c0a49f80
Thomas Rausch <thomas@rauschig.org>
2019-09-03 06:33:56 -0400

    Create multi-arch Docker build

---
+++ docker-build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+VERSION=0.0.1
+
+# registry/group/repository/image
+IMAGE=git.dsg.tuwien.ac.at:5005/mc2/galileo/galileo
+
+docker run --rm --privileged multiarch/qemu-user-static:register --reset
+
+docker build -t ${IMAGE}:${VERSION}-amd64 -f Dockerfile.amd64 .
+docker build -t ${IMAGE}:${VERSION}-arm32v7 -f Dockerfile.arm32v7 .
+
+docker push ${IMAGE}:${VERSION}-amd64
+docker push ${IMAGE}:${VERSION}-arm32v7
+
+docker manifest create --amend ${IMAGE}:${VERSION} \
+	${IMAGE}:${VERSION}-amd64 \
+	${IMAGE}:${VERSION}-arm32v7
+
+docker manifest push ${IMAGE}:${VERSION}
