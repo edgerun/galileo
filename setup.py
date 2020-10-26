2e4d40a10622f13e024d9ed8ecaeff9b62263e62
Thomas Rausch <thomas@rauschig.org>
2020-10-26 19:27:07 +0100

    Release version v0.7.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="edgerun-galileo",
-    version="0.7.0.dev1",
+    version="0.7.0",
     author="Thomas Rausch",
     author_email="t.rausch@dsg.tuwien.ac.at",
     description="Galileo: A framework for distributed load testing experiments",
